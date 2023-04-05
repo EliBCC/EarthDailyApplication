2875a35ed86120a46dbf16d4942e30d6f7ddd903
horizon.eli07@gmail.com <E2&p&JX4yIH2q0DEn322mH$9IK7Kjgog>
2023-04-04 17:27:27 -0700

    Added test cases

--- StringMerge.py
+++ StringMerge.py
@@ -1,3 +1,5 @@
+from collections import Counter                              # To check if lists of strings are equal
+
 # Solution
 def mergeLists(originalList, addList, rmList):
     pass
@@ -10,4 +12,43 @@ tests = [\
         "delete": ['two', 'five'],
         "answer": ['three', 'six', 'one']
     },
+    {
+        "original": ['two', 'three'],
+        "add": [],
+        "delete": [],
+        "answer": ['three', 'two']
+    },
+    {
+        "original": ['three', 'two'],
+        "add": [],
+        "delete": [],
+        "answer": ['three', 'two']
+    },
+    {
+        "original": [],
+        "add": ['two', 'three'],
+        "delete": [],
+        "answer": ['three', 'two']
+    },
+    {
+        "original": ['two', 'three'],
+        "add": [],
+        "delete": ['four'],
+        "answer": ['three', 'two']
+    },
+    {
+        "original": [],
+        "add": [],
+        "delete": [],
+        "answer": []
+    },
 ]
+
+def runTests():
+    for test in tests:
+        result = mergeLists(test["original"], test["add"], test["delete"])
+        if Counter(result) == Counter(test["answer"]):
+            print("Test passed")
+        else:
+            print("Test failed.\nCase: {}\nResult: {}".format(test, result))
+        
