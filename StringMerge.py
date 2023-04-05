d8d6677f6f6fbc4f64a0bb69146c4aea177317a6
horizon.eli07@gmail.com <E2&p&JX4yIH2q0DEn322mH$9IK7Kjgog>
2023-04-04 17:16:25 -0700

    Ititialize StringMerge.py

---
+++ StringMerge.py
@@ -0,0 +1,13 @@
+# Solution
+def mergeLists(originalList, addList, rmList):
+    pass
+
+# Tests
+tests = [\
+    {
+        "original": ['one', 'two', 'three'],
+        "add": ['one', 'two', 'five', 'six'],
+        "delete": ['two', 'five'],
+        "answer": ['three', 'six', 'one']
+    },
+]
