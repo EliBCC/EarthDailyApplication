5a68ebad9b355168569f0bb793b36b1baa5732bf
horizon.eli07@gmail.com <E2&p&JX4yIH2q0DEn322mH$9IK7Kjgog>
2023-04-05 10:20:00 -0700

    Find correct strings is working

--- StringMerge.py
+++ StringMerge.py
@@ -1,8 +1,21 @@
 from collections import Counter                              # To check if lists of strings are equal
+import pandas as pd
 
 # Solution
 def mergeLists(originalList, addList, rmList):
-    pass
+    # Create dataframes from lists
+    originalDF = pd.DataFrame(originalList, columns = ["strings"])
+    addDF = pd.DataFrame(addList, columns = ["strings"])
+
+    # Create dataframe with correct values
+    ans = originalDF                                                            # Create original list as Pandas dataframe
+    if len(originalList) == 0:
+        ans = addDF                                                             # If the original list was blank, use the add list instead
+    elif len(addList) > 0:
+        ans = ans.merge(addDF, how='outer')                                     # If both the original list and the add list contain items, merge them
+    ans = ans[~ans.isin(rmList)].dropna()                                       # Drop strings from rmList
+    
+    return ans["strings"].tolist()
 
 # Tests
 tests = [\
@@ -51,4 +64,5 @@ def runTests():
             print("Test passed")
         else:
             print("Test failed.\nCase: {}\nResult: {}".format(test, result))
-        
+
+runTests()
